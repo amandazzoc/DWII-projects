182f2319c5f9f930cab7d85ad6fd2e245d1548df
Amanda Oliveira <amandaol7b@gmail.com>
2024-09-12 13:16:33 -0300

    projeto

--- Node.js/SistemaLoja/index.js
+++ Node.js/SistemaLoja/index.js
@@ -8,8 +8,8 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.get("/clientes/:cpf?", function (req, res) {
-  const cpf = req.params.cpf;
+app.get("/clientes", function (req, res) {
+
   const listaClientes = [
     { nome: "Amanda", cpf: "517.261.838-40", endereco: "Rua Sabiá, 80" },
     { nome: "Camila", cpf: "331.671.898-28", endereco: "Rua Sabiá, 80" },
@@ -31,7 +31,7 @@ app.get("/clientes/:cpf?", function (req, res) {
   ];
   res.render("clientes", {
     listaClientes: listaClientes,
-    cpf: cpf,
+
   });
 });
 
@@ -39,18 +39,31 @@ app.get("/produtos/:produto?", function (req, res) {
     const produto = req.params.produto;
   const listaProdutos = [
     {
-      nome: "Capa Case para Kindle 10° Gen",
-      preco: 32.99,
-      categoria: "Variedades",
+      imagem: "/img/produto/loção.webp",
+      nome: "Loção Radiante Desodorante Corporal Nativa Spa Ameixa Dourada 100ml",
+      preco: 15.9,
+      categoria: "Nativa SPA",
     },
     {
-      nome: "Agulha de Crochê Tunisiano",
-      preco: 16.75,
-      categoria: "Armarinho",
+      nome: "Creme Nutritivo Firmador Resveratrol E Silício 40g",
+      preco: 131.92,
+      categoria: "Botik",
     },
-    { nome: "Lovito Macacão casual", preco: 64.45, categoria: "Roupas" },
-    { nome: "Máquina de Donuts", preco: 44.99, categoria: "Cozinha" },
-    { nome: "Linha Hauskraft Coleção Oasis", preco: 48.99, categoria: "Casa" },
+    {
+      nome: "African Sunrise Floral Frutal Eau De Parfum 75ml",
+      preco: 169.9,
+      categoria: "Botica 214",
+    },
+    {
+      nome: "Gold Desodorante Colônia 75ml",
+      preco: 119.9,
+      categoria: "Floratta",
+    },
+    {
+      nome: "Sérum Para Cílios e Sobrancelhas Explosion Grow 5g",
+      preco: 51.90,
+      categoria: "Make B.",
+    },
   ];
     res.render("produtos", {
         listaProdutos: listaProdutos,
@@ -61,12 +74,12 @@ app.get("/produtos/:produto?", function (req, res) {
 app.get("/pedidos/:id?", function (req, res) {
   const id = req.params.id;
   const listaPedidos = [
-    { numeroPedido: 1, valor: 32.99 },
-    { numeroPedido: 2, valor: 33.5 },
-    { numeroPedido: 3, valor: 81.2 },
-    { numeroPedido: 4, valor: 97.98 },
-    { numeroPedido: 5, valor: 49.74 },
-    { numeroPedido: 6, valor: 93.98 },
+    { numeroPedido: 1, valor: 199.90 },
+    { numeroPedido: 2, valor: 26.90 },
+    { numeroPedido: 3, valor: 144.90 },
+    { numeroPedido: 4, valor: 62.80 },
+    { numeroPedido: 5, valor: 34.904 },
+    { numeroPedido: 6, valor: 124.70 },
   ];
   res.render("pedidos", {
     listaPedidos: listaPedidos,
